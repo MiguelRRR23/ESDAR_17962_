bfa9be581a29b8ec6a850cddbb7976000ff72fba
MiguelRRR23 <miguelrrr816@gmail.com>
2025-03-27 17:46:08 -0500

    Vagones

---
+++ ListaenlazadaS/Locomotora.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaenlazadaS
+{
+    class Locomotora
+    {
+        Vagón Primero {  get; set; }
+
+        public void AgregaPrimero(int valor)
+        {
+            Vagón nuevo = new Vagón(valor);
+            if (this.Primero == null) // caso 1 locomotora sola
+            {
+                this.Primero = nuevo;
+
+            }
+            else //caso 2
+            {
+                Vagón primVag = this.Primero;
+
+                nuevo.sig = this.Primero;
+
+                this.Primero = nuevo;
+
+                
+            }
+
+        }
+
+
+        public void AgregaFinal(int valor)
+      {
+            Vagón nuevo = new Vagón(valor);
+            if (this.Primero == null) // caso 1 locomotora sola
+            {
+                this.Primero = nuevo;
+
+            }
+            else 
+            {
+                Vagón tmp = this.Primero;
+                while (tmp.sig != null)
+                {
+                    tmp = tmp.sig;
+
+                }
+                tmp.sig = nuevo;
+            }
+       }
+        public string VerVagones ()
+        {
+            string listaVag = "";
+            Vagón tmp = this.Primero;
+            while (tmp != null) {
+                listaVag += tmp.Dato + ",  ";
+                tmp = tmp.sig; 
+            }
+
+            return listaVag;
+        }
+
+    }
+}
---
+++ ListaenlazadaS/Vagón.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaenlazadaS
+{
+    class Vagón
+    {
+        public int Dato { get; set; }
+        public Vagón sig {  get; set; }
+
+        public Vagón(int dato)
+        {
+            Dato = dato;
+        }
+
+        public Vagón()
+        {
+        }
+    }
+}
